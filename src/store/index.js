0b41405c3fdc8f0e99127afc5b819c669f4b5bd9
daniel <lesamessipa@gmail.com>
2025-06-18 11:27:40 +0200

    first commit with axios

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import { createStore } from "vuex";
+
+import state from "./state";
+import actions from "./actions";
+import mutations from "./mutations";
+import getters from "./getters";
+
+const store = createStore({
+ // actions,
+  state,
+  //getters,
+ // mutations,
+});
+
+export default store;
---
+++ src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>home view</h1>
+    <div class="flex flex-col items-center p-8 justify-center">
+      <input
+        type="text"
+        class="rounded border-2 border-gray-200 w-full p-4"
+        placeholder="sreach for meals"
+      />
+
+      <div class="mt-2 flex justify-center gap-2 space-x-2">
+        <router-link
+          :to="{ name: 'byLetter', params: { letter } }"
+          v-for="letter of letters"
+          :key="letter"
+        >
+          {{ letter }}
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed, onMounted } from "vue";
+import store from "../store";
+import axiosClient from "../axiosClient";
+
+const meals = computed(() => store.state.meals);
+const letters = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z".split(
+  ","
+);
+onMounted( async () => {
+  const response = await axiosClient.get('/list.php?i=list')
+
+  console.log(response.data)
+})
+</script>
